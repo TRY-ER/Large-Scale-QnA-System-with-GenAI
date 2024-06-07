bd236f1f784b169b89f8a1741bc7b8d31a91bc70
TRY-ER <75255871+TRY-ER@users.noreply.github.com>
2024-06-07 11:43:28 +0530

    Delete src directory

--- src/components/Utils/Other.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Array2Dict = (Array:Array<any>) => {
-    const dictionary = Array.reduce((acc, item) => {
-        acc[item.Name] = item.Value;
-        return acc;
-    }, {});
-    return dictionary;
-}
